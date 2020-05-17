8b4d2ce6f5337ba50722b22514b33a1a13a5e549
yt8492 <tmymyut2000@gmail.com>
2020-05-18 03:59:20 +0900

    add emoji repository

---
+++ repository/emoji_repository.go
@@ -0,0 +1,29 @@
+package repository
+
+import "discord-slot/model"
+
+type EmojiRepository interface {
+	Save(emoji model.Emoji)
+	GetAll() []model.Emoji
+}
+
+type InMemoryEmojiRepository struct {
+	emojis *[]model.Emoji
+}
+
+func (r *InMemoryEmojiRepository) Save(emoji model.Emoji) {
+	for _, v := range *r.emojis{
+		if v == emoji {
+			return
+		}
+	}
+	*r.emojis = append(*r.emojis, emoji)
+}
+
+func (r *InMemoryEmojiRepository) GetAll() []model.Emoji {
+	return *r.emojis
+}
+
+func NewInMemoryEmojiRepository() InMemoryEmojiRepository {
+	return InMemoryEmojiRepository{&[]model.Emoji{}}
+}
