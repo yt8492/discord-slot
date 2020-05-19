a455b181f1b1ec77c819f3efcabc5b6787e77020
yt8492 <tmymyut2000@gmail.com>
2020-05-19 09:37:04 +0900

    add service

--- service/emoji_service.go
+++ service/emoji_service.go
@@ -3,12 +3,31 @@ package service
 import (
 	"discord-slot/model"
 	"discord-slot/repository"
+	"math/rand"
 )
 
-type EmojiRegisterService struct {
+type EmojiService struct {
 	er repository.EmojiRepository
 }
 
-func (s *EmojiRegisterService) RegisterEmoji(emoji model.Emoji) {
+func (s *EmojiService) RegisterEmoji(emoji model.Emoji) {
 	s.er.Save(emoji)
+}
+
+func (s *EmojiService) GetAllEmoji() []model.Emoji {
+	return s.er.GetAll()
+}
+
+func (s *EmojiService) GetRandomEmojis(n int) []model.Emoji {
+	src := s.er.GetAll()
+	retVal := make([]model.Emoji, n)
+	for i := 0; i < n; i++ {
+		emoji := src[rand.Intn(len(src))]
+		retVal = append(retVal, emoji)
+	}
+	return retVal
+}
+
+func (s *EmojiService) DeleteEmoji(emoji model.Emoji) {
+	s.er.Delete(emoji)
 }
