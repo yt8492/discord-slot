3e5a6b9e4e0a620484493614504e7d694b53d2e2
yt8492 <tmymyut2000@gmail.com>
2020-05-23 22:50:23 +0900

    use config.ini

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module discord-slot
 
 go 1.13
 
-require github.com/bwmarrin/discordgo v0.20.3
+require (
+	github.com/bwmarrin/discordgo v0.20.3
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+	gopkg.in/ini.v1 v1.56.0
+)
--- main.go
+++ main.go
@@ -3,55 +3,65 @@ package main
 import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"gopkg.in/ini.v1"
 	"log"
 	"math/rand"
 	"os"
 	"os/signal"
-	"regexp"
 	"strings"
 	"syscall"
 )
 
+type Config struct {
+	Prefix string
+	DiscordAuthenticationToken string
+}
+
+var Conf Config
+
+func init() {
+	c, _ := ini.Load("config.ini")
+	Conf = Config{
+		Prefix: c.Section("config").Key("prefix").MustString("$"),
+		DiscordAuthenticationToken: c.Section("config").Key("token").String(),
+	}
+}
+
 func main() {
-	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_AUTHENTICATION_TOKEN"))
+	discord, err := discordgo.New("Bot " + Conf.DiscordAuthenticationToken)
 	if err != nil {
 		log.Fatal(err)
 	}
-	prefix := "!testbot"
+	prefix := Conf.Prefix
 	discord.AddHandler(func(s *discordgo.Session, mc *discordgo.MessageCreate) {
 		log.Println(mc.Content)
-		if mc.Author.ID == s.State.User.ID || !strings.HasPrefix(mc.Content, "!testbot") {
+		if mc.Author.ID == s.State.User.ID || !strings.HasPrefix(mc.Content, prefix) {
 			return
 		}
-		texts := tokenize(mc.Content)[1:]
-		if len(texts) > 0 {
-			command := texts[0]
-			var g *discordgo.Guild
-			g, err = s.Guild(mc.GuildID)
-			if err != nil {
-				log.Println(err)
-				return
+		command := strings.TrimPrefix(mc.Content, prefix)
+		var g *discordgo.Guild
+		g, err = s.Guild(mc.GuildID)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		switch command {
+		case "slot":
+			emojis := getGuildEmojis(g)
+			slot := make([]string, 3)
+			for i := 0; i < 3; i++ {
+				slot[i] = emojis[rand.Intn(len(emojis))]
 			}
-			switch command {
-			case "slot":
-				emojis := getGuildEmojis(g)
-				slot := make([]string, 3)
-				for i := 0; i < 3; i++ {
-					slot[i] = emojis[rand.Intn(len(emojis))]
-				}
-				var result string
-				if len(slot) > 0 {
-					result = strings.Join(slot, "")
-				} else {
-					result = "This guild has no emoji."
-				}
-				_, err = s.ChannelMessageSend(mc.ChannelID, result)
-			case "list":
-				emojis := getGuildEmojis(g)
-				_, err = s.ChannelMessageSend(mc.ChannelID, strings.Join(emojis, "\n"))
+			var result string
+			if len(slot) > 0 {
+				result = strings.Join(slot, "")
+			} else {
+				result = "This guild has no emoji."
 			}
-		} else {
-			_, err = s.ChannelMessageSend(mc.ChannelID, fmt.Sprintf("usage: %s [command name] [arguments..]", prefix))
+			_, err = s.ChannelMessageSend(mc.ChannelID, result)
+		case "list":
+			emojis := getGuildEmojis(g)
+			_, err = s.ChannelMessageSend(mc.ChannelID, strings.Join(emojis, "\n"))
 		}
 		if err != nil {
 			log.Print(err)
@@ -69,12 +79,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func tokenize(s string) []string {
-	re := regexp.MustCompile(`\s+`)
-	splited := re.Split(s, -1)
-	return splited
 }
 
 func getGuildEmojis(g *discordgo.Guild) []string {
